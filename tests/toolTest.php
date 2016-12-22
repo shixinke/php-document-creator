5573e10be72af15c6e9f0da094d633ac001be38a
shixinke <ishixinke@qq.com>
2016-12-22 18:24:34 +0800

    增加字典

---
+++ tests/toolTest.php
@@ -0,0 +1,11 @@
+<?php
+define('ROOT_PATH', dirname(__DIR__));
+require_once ROOT_PATH.'/Autoloader.php';
+
+use phpDocumentCreator\Tool as Tool;
+var_dump(Tool::getConstants(new ReflectionExtension('Swoole')));
+
+
+
+
+
--- tests/transformTest.php
+++ tests/transformTest.php
@@ -9,7 +9,7 @@ $transform = new \phpDocumentCreator\Transform('./notes/redis.php');
 $data = $transform->info('X', '');
 $doc->updateDict('redis', $data);
 
-$doc->create();
+//$doc->create();
 */
 
 /*
@@ -18,7 +18,7 @@ $transform = new \phpDocumentCreator\Transform('./notes/yaf.php');
 $data = $transform->info('X', '_');
 $doc->updateDict('yaf', $data);
 
-$doc->create();
+//$doc->create();
 */
 
 /*
@@ -32,11 +32,25 @@ $doc->create();
 
 /*
 $doc = new \phpDocumentCreator\Document('swoole');
+$doc->createDict();
+*/
+
+/*
+$doc = new \phpDocumentCreator\Document('swoole');
 $transform = new \phpDocumentCreator\Transform(__DIR__.'/notes/swoole.php');
 $data = $transform->info('X', "_");
 $doc->updateDict('swoole', $data);
-$doc->create();
+//$doc->create();
 */
+
+$doc = new \phpDocumentCreator\Document('yar');
+$doc->createDict();
+
+$doc = new \phpDocumentCreator\Document('yac');
+$doc->createDict();
+
+$doc = new \phpDocumentCreator\Document('yaconf');
+$doc->createDict();
 
 
 
